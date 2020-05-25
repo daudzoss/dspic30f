547f242387088f74a524ef19643995f2762baab3
Daud Zoss <daudzoss@gmail.com>
2020-05-24 23:30:34 -0700

    tested and refined, for v3_02 and v3_31

--- build.sh
+++ build.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
+# $0          grabs default package off the web, installs in default directory
+# $0 -V       uses tarball in current directory, installs in default directory
+# $0 -V $HOME uses tarball in current directory, installs in home directory
+#
+# Note: After v3_02, Microchip broke wget scripts by requiring a credential to
+#       download the GPL source code(!)
+#       As a workaround, save the gzip'ed tarball in the current directory and
+#       use the -V flag as shown above.
 fromdos -V || exit 1
 msgfmt -V || exit 2
-if $#
+if [ $# ]
 then
- wget $1 || echo "only arg should be URL to an mplabalc30 tarball, if not v3_31"
+ wget $1 || echo "arg1 may be -V, or URL to an mplabalc30 tarball if not v3_02"
 else
- wget http://ww1.microchip.com/downloads/en/DeviceDoc/mplabalc30v3_31.tgz || exit 3
+ wget http://ww1.microchip.com/downloads/en/DeviceDoc/mplabalc30v3_02.tar.gz
 fi
-tar xvzf *.t*gz || exit 4
+tar xvzf *.t*gz || exit 3
 for dosfile in $(find acme -print); do fromdos $dosfile 2> /dev/null; done
 cd acme
-./configure pic30-unknown-elf # change to pic30-unknown-coff if old-style format
+./configure pic30-unknown-elf --prefix=$2 # or, change to pic30-unknown-coff
 make # will fail for missing Makefile target
-echo "all:" > ./libiberty/testsuite/Makefile
-echo >& ./libiberty/testsuite/Makefile
-echo "install:" > ./libiberty/testsuite/Makefile
-echo >& ./libiberty/testsuite/Makefile
-make
-sudo make install
+cp ../Makefile.new libiberty/testsuite/Makefile
+make && make install # must run as root if installing under /usr/local
