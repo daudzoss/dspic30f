efe083cee9b10dc67a3298980acc0952b9ebe4f0
Daud Zoss <daudzoss@gmail.com>
2020-05-24 22:23:11 -0700

    verified to build and install (in /usr/local/pic30-unknown-elf)

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-#wget http://ww1.microchip.com/downloads/en/DeviceDoc/mplabalc30v2_05.tgz
-tar xvzf mplabalc30v2_05.tgz
+fromdos -V || exit 1
+msgfmt -V || exit 2
+if $#
+then
+ wget $1 || echo "only arg should be URL to an mplabalc30 tarball, if not v3_31"
+else
+ wget http://ww1.microchip.com/downloads/en/DeviceDoc/mplabalc30v3_31.tgz || exit 3
+fi
+tar xvzf *.t*gz || exit 4
 for dosfile in $(find acme -print); do fromdos $dosfile 2> /dev/null; done
 cd acme
-./configure pic30-unknown-elf
-make # will fail for missing Makefile
+./configure pic30-unknown-elf # change to pic30-unknown-coff if old-style format
+make # will fail for missing Makefile target
 echo "all:" > ./libiberty/testsuite/Makefile
+echo >& ./libiberty/testsuite/Makefile
+echo "install:" > ./libiberty/testsuite/Makefile
+echo >& ./libiberty/testsuite/Makefile
 make
+sudo make install
