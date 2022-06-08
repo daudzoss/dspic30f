1e65487a2bce6bb4816dd254edd55c8a76b45b5a
Daud Zoss <daudzoss@gmail.com>
2022-06-08 16:18:38 -0700

    new test cases for eventual add_any and mul_any

--- 14point/test.s
+++ 14point/test.s
@@ -8,8 +8,24 @@ _main:
 	add_r_n	w1,w0,w2,w3
 
 	mov	#0x0716,w3 	;
-	mov	#0x4000,w1	; w1 = 22.0/7.0;
+	sub	#0x0101,w3	;
 	uns_rat	w3,w3
+	mov	#0x4000,w1	; w1 = 22.0/7.0;
 	combine	w3,w1
-	xor	w2,w1,w0	; w0 = w1 ^ w2; // should be 0
+	xor	w2,w1,w8	; w8 = w1 ^ w2; // should be 0
 	
+	mov	#0x5663		;
+	sub	#0x0101,w0	; w0 = 99.0/70.0; // approx sqrt(3)
+	uns_rat	w0,w0
+;	mul_any	w0,w0,w2	; w2 = w0 * w0; // should be a ratio close to 3
+;	mul_any	w1,w2,w2	; w2 *= w1 /*pi*/; // should be between 9 and 10
+	
+	mov	#-9,w0		;
+	toratio	w0,w0
+;	add_any	w2,w0,w4
+	mov	w4,w9		; w9 = w2 - 9; // should be positive
+	mov	#-10,w0		;
+	toratio	w0,w0
+;	add_any	w2,w0,w4	;
+	mov	w4,w10		; w10 = w2 - 10; // should be negative
+
