aa94f1c08de6b377976777d2595d1d50ff99bd9a
Daud Zoss <daudzoss@gmail.com>
2022-06-24 10:28:30 -0700

    debugging on 14-hour-delayed AAL2410

--- 14point/test.s
+++ 14point/test.s
@@ -15,11 +15,13 @@ _main:
 	combine	w3,w1
 	xor	w2,w1,w8	; w8 = w1 ^ w2; // should be 0
 	
-	mov	#0x5663,w0	;
-	sub	#0x0101,w0	; w0 = 99.0/70.0; // approx sqrt(3)
+	mov	#0x4663,w0	;
+	sub	#0x0101,w0	;
 	uns_rat	w0,w0
-	mul_any	w0,w0,w2,w3,w4,w5
-	mov	w2,w0		; w0 = w0 * w0; // should be a ratio close to 3
+	bset	w0,#FORMATB	; w0 = 99.0/70.0; // approx sqrt(3)
+	mov	w0,w6		;
+	mul_any	w0,w6,w2,w3,w4,w5
+	mov	w2,w0		; w0 = w0 * w0; // should be a ratio close to 3 [20220624: but instead is 0x6f49 == 73/94]
 	mul_any	w1,w2,w2,w3,w4,w5; w2 *= w1 /*pi*/; // should be between 9 and 10
 	
 	mov	#-9,w0		;
